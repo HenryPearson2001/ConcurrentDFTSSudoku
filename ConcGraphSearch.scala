5deae4886b3a442a56985cea170f35b24f09f627
Henry Pearson <pearsonhenryj@icloud.com>
2021-02-24 12:32:44 +0000

    built concurrent DFTS

--- ConcGraphSearch.scala
+++ ConcGraphSearch.scala
@@ -7,16 +7,40 @@ class ConcGraphSearch[N](g: Graph[N]) extends GraphSearch[N](g){
 
     private val stack = TerminatingConcStack[(N, List[N])](numWorkers)
 
+    // channel which sends solution to coordinator
+    private val toCoordinator = ManyOne[List[N]]
+
+    // store the solution
+    private val result: Option[List[N]] = None
+
     // a single worker
-    private def worker = proc {
+    private def worker(isTarget: N => Boolean) = proc {
         repeat {
-            val (n, path) = stack.
+            // get the next node to search
+            val (n, path) = stack.pop
+            for (n1 <- g.succs(n)) {
+                // if solution found, send to coordinator
+                if isTarget(n1) toCoordinator!(path :+ n1)
+                // otherwise add to the stack
+                else stack.push((n1, path :+ n1))
+            }
         }
+        // close the coordinator
+        toCoordinator.close
+    }
+
+    // the coordinator
+    private def coordinator = proc {
+        attempt{ result = toCoordinator?() }{}
+        stack.shutdown
+        toCoordinator.close
     }
 
     /** Perform a depth-first search in g, starting from start, for a node that
       * satisfies isTarget. */
     def apply(start: N, isTarget: N => Boolean): Option[N] = {
-
+        val workers = || (for (_ <- 0 until numWorkers) yield worker)
+        run(workers || coordinator)
+        result
     }
 }
